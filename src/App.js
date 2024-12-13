87a084cc5f9557800209789e9b1965cb05ed069f
yenurkar-ganesh <ganeshyenurkar4@gmail.com>
2024-12-13 08:15:56 +0000

    tasty-kitchent

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css'
+
+const sortByOptions = [
+  {
+    id: 0,
+    displayText: 'Highest',
+    value: 'Highest',
+  },
+  {
+    id: 2,
+    displayText: 'Lowest',
+    value: 'Lowest',
+  },
+]
+
+const App = () => <div>Hello World</div>
+
+export default App
